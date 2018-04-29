ed7ab99541fd0d893b2c1c963aa89271f6e8402c
Xxinnn <32243584+Xxinnn@users.noreply.github.com>
2018-04-29 12:06:06 -0400

    Add files via upload

--- js/d3.js
+++ js/d3.js
@@ -1,15 +1,19 @@
-//import csv
-    
+//var container = document.getElementById("gallery");
+
 var csv_id=[];
 var csv_artworkName=[];
 var csv_time=[];
 var csv_description=[];
+var csv_artistName=[];
 var csv_size_cm=[];
 
-d3.csv("imageID.csv",function(csv){
+//import csv
+function read_csv() {
+    d3.csv("imageID.csv",function(csv){
             csv.map(function(d){
                 csv_id.push(+d.ID);
                 csv_artworkName.push(d.artworkName);
+                csv_artistName.push(d.artistName);
                 csv_time.push(d.time);
                 csv_description.push(d.description);
                 csv_size_cm.push(d.size_cm);
@@ -19,8 +23,4 @@ d3.csv("imageID.csv",function(csv){
 //            console.log("artwortName",artworkName);
 //            console.log("id",id[0]);
         });
-
-
-
-
-
+}
