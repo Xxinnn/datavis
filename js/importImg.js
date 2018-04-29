1f3a84c418bbf503273146822909b685c3be669b
Xxinnn <32243584+Xxinnn@users.noreply.github.com>
2018-04-29 12:03:34 -0400

    Add files via upload

--- js/importImg.js
+++ js/importImg.js
@@ -1,26 +1,28 @@
-//var container = document.getElementById("gallery");
-var csv_id=[];
-var csv_artworkName=[];
-var csv_time=[];
-var csv_description=[];
-var csv_artistName=[];
-var csv_size_cm=[];
+////var container = document.getElementById("gallery");
+//var csv_id=[];
+//var csv_artworkName=[];
+//var csv_time=[];
+//var csv_description=[];
+//var csv_artistName=[];
+//var csv_size_cm=[];
+//
+////import csv
+//d3.csv("imageID.csv",function(csv){
+//            csv.map(function(d){
+//                csv_id.push(+d.ID);
+//                csv_artworkName.push(d.artworkName);
+//                csv_artistName.push(d.artistName);
+//                csv_time.push(d.time);
+//                csv_description.push(d.description);
+//                csv_size_cm.push(d.size_cm);
+//            })
+//            //called after the AJAX is success
+////            console.log("id",id);
+////            console.log("artwortName",artworkName);
+////            console.log("id",id[0]);
+//        });
+    read_csv();
 
-//import csv
-d3.csv("imageID.csv",function(csv){
-            csv.map(function(d){
-                csv_id.push(+d.ID);
-                csv_artworkName.push(d.artworkName);
-                csv_artistName.push(d.artistName);
-                csv_time.push(d.time);
-                csv_description.push(d.description);
-                csv_size_cm.push(d.size_cm);
-            })
-            //called after the AJAX is success
-//            console.log("id",id);
-//            console.log("artwortName",artworkName);
-//            console.log("id",id[0]);
-        });
 
 var row1 = document.querySelector('.row1');
 var row2 = document.querySelector('.row2');
@@ -42,6 +44,7 @@ var i=0;
 //the first and second row has to show two images at the same time when keydown once so consider urls1[3] and urls2[3]
 
 $(document).ready(function() {
+
 $(document).keydown(function(e){
     if(e.keyCode==39 && i == 3) {
 //        import img
@@ -54,19 +57,30 @@ $(document).keydown(function(e){
 //        set src and id to image imported
         newImage1.setAttribute('src', 'img/postImpressionism/' + urls1[i] + '.jpg');
         newImage1.setAttribute('id',urls1[i]);
+        newImage1.setAttribute('title',csv_artworkName[urls1[i]-281]);
+
 
     
         newImage2.setAttribute('src', 'img/postImpressionism/' + urls2[i] + '.jpg');
         newImage2.setAttribute('id',urls2[i]);
+        newImage2.setAttribute('title',csv_artworkName[urls2[i]-281]);
+        console.log(newImage2);
         
         newImage4.setAttribute('src', 'img/postImpressionism/' + urls1[i+1] + '.jpg');
         newImage4.setAttribute('id',urls1[i+1]);
+        newImage4.setAttribute('title',csv_artworkName[urls1[i+1]-281]);
+        console.log(newImage4);
+        
         newImage5.setAttribute('src', 'img/postImpressionism/' + urls2[i+1] + '.jpg');
         newImage5.setAttribute('id',urls2[i+1]);
+        newImage5.setAttribute('title',csv_artworkName[urls2[i+1]-281]);
+        console.log(newImage5);
 
         
         newImage3.setAttribute('src', 'img/postImpressionism/' + urls3[i] + '.jpg');
         newImage3.setAttribute('id',urls3[i]);
+        newImage3.setAttribute('title',csv_artworkName[urls3[i]-281]);
+        console.log(newImage3);
 
 //        show img
         row1.append(newImage1);
@@ -85,15 +99,21 @@ $(document).keydown(function(e){
             
         newImage1.setAttribute('src', 'img/postImpressionism/' + urls1[i] + '.jpg');
         newImage1.setAttribute('id',urls1[i]);
+        newImage1.setAttribute('title',csv_artworkName[urls1[i]-281]);
+        console.log(newImage1);
 
     
         newImage2.setAttribute('src', 'img/postImpressionism/' + urls2[i] + '.jpg');
         newImage2.setAttribute('id',urls2[i]);
+        newImage2.setAttribute('title',csv_artworkName[urls2[i]-281]);
+        console.log(newImage2);
 
 
         
         newImage3.setAttribute('src', 'img/postImpressionism/' + urls3[i] + '.jpg');
         newImage3.setAttribute('id',urls3[i]);
+        newImage3.setAttribute('title',csv_artworkName[urls3[i]-281]);
+        console.log(newImage3);
 
         
         row1.append(newImage1);
@@ -107,8 +127,6 @@ $(document).keydown(function(e){
     var td;
     var t_id;
     var callImage = document.createElement('img');
-
-    var detail_img=document.querySelector('#detail_img');
     
     for (var t = 1; t < 21; t++){
         
@@ -117,32 +135,28 @@ $(document).keydown(function(e){
         //hover tooltip
         
         
-        $("td").mouseover(function(event) {
+        $(td).mouseover(function(event) {
             hoverId=event.target.id;
-            callImage.setAttribute('title',csv_artworkName[hoverId-281]);
-            $("img[title]").tooltip();
+//           console.log(hoverId);
+//            callImage.setAttribute('title',csv_artworkName[hoverId-281]);
+            console.log(callImage);
+            $("img[title]").tooltip( {
+      position: {
+        my: "center bottom-20",
+        at: "center top",
+        using: function( position, feedback ) {
+          $( this ).css( position );
+          $( "<div>" )
+            .addClass( "arrow" )
+            .addClass( feedback.vertical )
+            .addClass( feedback.horizontal )
+            .appendTo( this );
+        }
+      }
+    }
+            );
         });
-//         $(td).mouseover(function(event) {
-//            createTooltip(event);               
-//         }).mouseout(function(){
-//    // create a hidefunction on the callback if you want
-//            hideTooltip(); 
-//         });
-//
-//        function createTooltip(event){          
-//            $('<div class="tooltip">test</div>').appendTo('body');
-//            positionTooltip(event);        
-//        }
-//        
-//        function hideTooltip(event){
-//            $('<div class="tooltip">test</div>').remove();
-//        }
-//
-//        function positionTooltip(event){
-//            var tPosX = event.pageX - 10;
-//            var tPosY = event.pageY - 100;
-//            $('div.tooltip').css({'position': 'absolute', 'top': tPosY + 'px', 'left': tPosX+'px'});
-//        }
+
 
         
         //open and close detail window
@@ -169,11 +183,11 @@ $(document).keydown(function(e){
                     $("#layer").fadeOut(300);
                     $("#detail").css("display", "none");
                  
-                 callImage.removeAll();
-                    $("#name").removeChild();
-                    $("#artist").removeChild();
-                    $("#description").removeChild();
-                    $("#size").removeChild();
+                 callImage.removeAll;
+                    $("#name").removeChild;
+                    $("#artist").removeChild;
+                    $("#description").removeChild;
+                    $("#size").removeChild;
              })      
         })
 
